a2b994d935860e7d89a9f7a98c53572bc93b4a53
root <maddyyfleming@gmail.com>
2025-09-05 16:55:10 +1000

    again

--- debugging/print_arguments.py
+++ debugging/print_arguments.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 import sys
 
-print("print_arguments.py")
-for arg in sys.argv[1:]: 
-    print(arg)
+for i in range(len(sys.argv)):
+    print(sys.argv[i])
---
+++ debugging/print_arguments_correction.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+import sys
+import os
+
+print(os.path.basename(sys.argv[0]))
+for arg in sys.argv[1:]: 
+    print(arg)
