08898c4c196e35000435146def758a9346c72f8e
root <maddyyfleming@gmail.com>
2025-09-05 16:33:57 +1000

    printagain

--- debugging/print_arguments.py
+++ debugging/print_arguments.py
@@ -2,11 +2,9 @@
 import sys
 import os
 
-def main():
+def main(argv):
     print(os.path.basename(sys.argv[0]))
     for arg in sys.argv[1:]: 
         print(arg)
 
-
-if __name__ == "__main__":
-    main()
+main(sys.argv)
